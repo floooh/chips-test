aa643b18e0bfc26a958ba8b60a28594a68ba1e2a
Andre Weissflog <floooh@gmail.com>
2017-11-18 21:57:39 +0800

    more load tests

--- tests/z80-test.c
+++ tests/z80-test.c
@@ -50,8 +50,13 @@ uint32_t step() {
 
 #define T(x) assert(x)
 
+/* LD A,R; LD A,I */
+void LD_a_ri() {
+    /* FIXME */
+}
+
 /* LD r,s; LD r,n */
-void ld_r_sn() {
+void LD_r_sn() {
     uint8_t prog[] = {
         0x3E, 0x12,     // LD A,0x12
         0x47,           // LD B,A
@@ -177,8 +182,140 @@ void ld_r_sn() {
     T(4==step()); T(0x18==cpu.A);
 }
 
+/* LD r,(HL) */
+void LD_r_iHLi() {
+    uint8_t prog[] = {
+        0x21, 0x00, 0x10,   // LD HL,0x1000
+        0x3E, 0x33,         // LD A,0x33
+        0x77,               // LD (HL),A
+        0x3E, 0x22,         // LD A,0x22
+        0x46,               // LD B,(HL)
+        0x4E,               // LD C,(HL)
+        0x56,               // LD D,(HL)
+        0x5E,               // LD E,(HL)
+        0x66,               // LD H,(HL)
+        0x26, 0x10,         // LD H,0x10
+        0x6E,               // LD L,(HL)
+        0x2E, 0x00,         // LD L,0x00
+        0x7E,               // LD A,(HL)
+    };
+    init();
+    copy(0x0000, prog, sizeof(prog));
+
+    T(10==step()); T(0x1000 == cpu.HL);
+    T(7==step()); T(0x33 == cpu.A);
+    T(7==step()); T(0x33 == mem[0x1000]);
+    T(7==step()); T(0x22 == cpu.A);
+    T(7==step()); T(0x33 == cpu.B);
+    T(7==step()); T(0x33 == cpu.C);
+    T(7==step()); T(0x33 == cpu.D);
+    T(7==step()); T(0x33 == cpu.E);
+    T(7==step()); T(0x33 == cpu.H);
+    T(7==step()); T(0x10 == cpu.H);
+    T(7==step()); T(0x33 == cpu.L);
+    T(7==step()); T(0x00 == cpu.L);
+    T(7==step()); T(0x33 == cpu.A);       
+}
+
+/* LD r,(IX|IY+d) */
+void LD_r_iIXIYi() {
+    /* FIXME */
+}
+
+/* LD (HL),r */
+void LD_iHLi_r() {
+    uint8_t prog[] = {
+        0x21, 0x00, 0x10,   // LD HL,0x1000
+        0x3E, 0x12,         // LD A,0x12
+        0x77,               // LD (HL),A
+        0x06, 0x13,         // LD B,0x13
+        0x70,               // LD (HL),B
+        0x0E, 0x14,         // LD C,0x14
+        0x71,               // LD (HL),C
+        0x16, 0x15,         // LD D,0x15
+        0x72,               // LD (HL),D
+        0x1E, 0x16,         // LD E,0x16
+        0x73,               // LD (HL),E
+        0x74,               // LD (HL),H
+        0x75,               // LD (HL),L
+    };
+    init();
+    copy(0x0000, prog, sizeof(prog));
+
+    T(10==step()); T(0x1000 == cpu.HL);
+    T(7==step()); T(0x12 == cpu.A);
+    T(7==step()); T(0x12 == mem[0x1000]);
+    T(7==step()); T(0x13 == cpu.B);
+    T(7==step()); T(0x13 == mem[0x1000]);
+    T(7==step()); T(0x14 == cpu.C);
+    T(7==step()); T(0x14 == mem[0x1000]);
+    T(7==step()); T(0x15 == cpu.D);
+    T(7==step()); T(0x15 == mem[0x1000]);
+    T(7==step()); T(0x16 == cpu.E);
+    T(7==step()); T(0x16 == mem[0x1000]);
+    T(7==step()); T(0x10 == mem[0x1000]);
+    T(7==step()); T(0x00 == mem[0x1000]);
+}
+
+/* LD (IX|IY+d),r */
+void LD_iIXIYi_r() {
+    /* FIXME */
+}
+
+/* LD (HL),n */
+void LD_iHLi_n() {
+    uint8_t prog[] = {
+        0x21, 0x00, 0x20,   // LD HL,0x2000
+        0x36, 0x33,         // LD (HL),0x33
+        0x21, 0x00, 0x10,   // LD HL,0x1000
+        0x36, 0x65,         // LD (HL),0x65
+    };
+    init();
+    copy(0x0000, prog, sizeof(prog));
+
+    T(10==step()); T(0x2000 == cpu.HL);
+    T(10==step()); T(0x33 == mem[0x2000]);
+    T(10==step()); T(0x1000 == cpu.HL);
+    T(10==step()); T(0x65 == mem[0x1000]);
+}
+
+/* LD (IX|IY+d),n */
+void LD_iIXIYi_n() {
+    /* FIXME */
+}
+
+/* LD A,(BC); LD A,(DE); LD A,(nn) */
+void LD_A_iBCDEnni() {
+    uint8_t data[] = {
+        0x11, 0x22, 0x33
+    };
+    copy(0x1000, data, sizeof(data));
+    uint8_t prog[] = {
+        0x01, 0x00, 0x10,   // LD BC,0x1000
+        0x11, 0x01, 0x10,   // LD DE,0x1001
+        0x0A,               // LD A,(BC)
+        0x1A,               // LD A,(DE)
+        0x3A, 0x02, 0x10,   // LD A,(0x1002)
+    };
+    copy(0x0000, prog, sizeof(prog));
+    init();
+
+    T(10==step()); T(0x1000 == cpu.BC);
+    T(10==step()); T(0x1001 == cpu.DE);
+    T(7 ==step()); T(0x11 == cpu.A);
+    T(7 ==step()); T(0x22 == cpu.A);
+    T(13==step()); T(0x33 == cpu.A);
+}
 
 int main() {
-    ld_r_sn();
+    LD_a_ri();
+    LD_r_sn();
+    LD_r_iHLi();
+    LD_r_iIXIYi();
+    LD_iHLi_r();
+    LD_iIXIYi_r();
+    LD_iHLi_n();
+    LD_iIXIYi_n();
+    LD_A_iBCDEnni();
     return 0;
 }
