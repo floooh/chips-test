24e05bdff788e88805592f4f9985e7ab61548b45
Andre Weissflog <floooh@gmail.com>
2017-12-25 11:16:33 +0100

    Fixed z80pio-test.c

--- tests/z80pio-test.c
+++ tests/z80pio-test.c
@@ -35,46 +35,46 @@ void test_read_write_control() {
     T(pio.reset_active);
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_A, 0xEE);
     T(!pio.reset_active);
-    T(pio.PORT[Z80PIO_PORT_A].int_vector == 0xEE);
-    T(pio.PORT[Z80PIO_PORT_A].int_control &= Z80PIO_INTCTRL_EI);
+    T(pio.port[Z80PIO_PORT_A].int_vector == 0xEE);
+    T(pio.port[Z80PIO_PORT_A].int_control &= Z80PIO_INTCTRL_EI);
 
     /* write interrupt vector 0xCC for port B */
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_B, 0xCC);
-    T(pio.PORT[Z80PIO_PORT_B].int_vector == 0xCC);
-    T(pio.PORT[Z80PIO_PORT_B].int_control &= Z80PIO_INTCTRL_EI);
+    T(pio.port[Z80PIO_PORT_B].int_vector == 0xCC);
+    T(pio.port[Z80PIO_PORT_B].int_control &= Z80PIO_INTCTRL_EI);
 
     /* set port A to output */
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_A, (Z80PIO_MODE_OUTPUT<<6)|0x0F);
-    T(pio.PORT[Z80PIO_PORT_A].mode == Z80PIO_MODE_OUTPUT);
+    T(pio.port[Z80PIO_PORT_A].mode == Z80PIO_MODE_OUTPUT);
 
     /* set port B to input */
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_B, (Z80PIO_MODE_INPUT<<6)|0x0F);
-    T(pio.PORT[Z80PIO_PORT_B].mode == Z80PIO_MODE_INPUT);
+    T(pio.port[Z80PIO_PORT_B].mode == Z80PIO_MODE_INPUT);
 
     /* set port A to bidirectional */
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_A, (Z80PIO_MODE_BIDIRECTIONAL<<6)|0x0F);
-    T(pio.PORT[Z80PIO_PORT_A].mode == Z80PIO_MODE_BIDIRECTIONAL);
+    T(pio.port[Z80PIO_PORT_A].mode == Z80PIO_MODE_BIDIRECTIONAL);
 
     /* set port A to mode control (plus followup io_select mask) */
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_A, (Z80PIO_MODE_BITCONTROL<<6)|0x0F);
-    T(!pio.PORT[Z80PIO_PORT_A].int_enabled);
-    T(pio.PORT[Z80PIO_PORT_A].mode == Z80PIO_MODE_BITCONTROL);
+    T(!pio.port[Z80PIO_PORT_A].int_enabled);
+    T(pio.port[Z80PIO_PORT_A].mode == Z80PIO_MODE_BITCONTROL);
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_A, 0xAA);
-    T(pio.PORT[Z80PIO_PORT_A].int_enabled);
-    T(pio.PORT[Z80PIO_PORT_A].io_select == 0xAA);
+    T(pio.port[Z80PIO_PORT_A].int_enabled);
+    T(pio.port[Z80PIO_PORT_A].io_select == 0xAA);
 
     /* set port B interrupt control word (with interrupt control mask following) */
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_B, (Z80PIO_INTCTRL_ANDOR|Z80PIO_INTCTRL_HILO|Z80PIO_INTCTRL_MASK_FOLLOWS)|0x07);
-    T(!pio.PORT[Z80PIO_PORT_B].int_enabled);
-    T(pio.PORT[Z80PIO_PORT_B].int_control == (Z80PIO_INTCTRL_ANDOR|Z80PIO_INTCTRL_HILO|Z80PIO_INTCTRL_MASK_FOLLOWS));
+    T(!pio.port[Z80PIO_PORT_B].int_enabled);
+    T(pio.port[Z80PIO_PORT_B].int_control == (Z80PIO_INTCTRL_ANDOR|Z80PIO_INTCTRL_HILO|Z80PIO_INTCTRL_MASK_FOLLOWS));
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_B, 0x23);
-    T(!pio.PORT[Z80PIO_PORT_B].int_enabled);
-    T(pio.PORT[Z80PIO_PORT_B].int_mask == 0x23);
+    T(!pio.port[Z80PIO_PORT_B].int_enabled);
+    T(pio.port[Z80PIO_PORT_B].int_mask == 0x23);
 
     /* enable interrupts on port B */
     _z80pio_write_ctrl(&pio, Z80PIO_PORT_B, Z80PIO_INTCTRL_EI|0x03);
-    T(pio.PORT[Z80PIO_PORT_B].int_enabled);
-    T(pio.PORT[Z80PIO_PORT_B].int_control == (Z80PIO_INTCTRL_EI|Z80PIO_INTCTRL_ANDOR|Z80PIO_INTCTRL_HILO|Z80PIO_INTCTRL_MASK_FOLLOWS));
+    T(pio.port[Z80PIO_PORT_B].int_enabled);
+    T(pio.port[Z80PIO_PORT_B].int_control == (Z80PIO_INTCTRL_EI|Z80PIO_INTCTRL_ANDOR|Z80PIO_INTCTRL_HILO|Z80PIO_INTCTRL_MASK_FOLLOWS));
 
     /* write interrupt control word to A and B, 
        and read the control word back, this does not
