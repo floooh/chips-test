184db6a3cb3e10f0989a4016f8bb302cb3d9f783
Andre Weissflog <floooh@gmail.com>
2017-12-31 16:39:37 +0100

    started with mc6847-test

---
+++ tests/mc6847-test.c
@@ -0,0 +1,29 @@
+//------------------------------------------------------------------------------
+//  mc6847-test.c
+//------------------------------------------------------------------------------
+// force assert() enabled
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#define CHIPS_IMPL
+#include "chips/mc6847.h"
+#include <stdio.h>
+
+uint32_t rgba8_buffer[MC6847_DISPLAY_WIDTH * MC6847_DISPLAY_HEIGHT];
+
+uint64_t fetch(uint64_t pins) {
+    return pins;
+}
+
+int main() {
+    mc6847_t vdg;
+    mc6847_desc_t desc = {
+        .tick_hz = 1000000,
+        .rgba8_buffer = rgba8_buffer,
+        .rgba8_buffer_size = sizeof(rgba8_buffer),
+        .fetch_cb = fetch
+    };
+    mc6847_init(&vdg, &desc);
+
+    return 0;
+}
