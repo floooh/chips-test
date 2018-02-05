6e416aabaf2fcf2a0a6280e8c74cd06bb79d04ee
Andre Weissflog <floooh@gmail.com>
2018-02-05 19:27:18 +0100

    Some simple read/write tests on the 6522

--- tests/m6522-test.c
+++ tests/m6522-test.c
@@ -22,19 +22,34 @@ uint8_t in_cb(int port_id) {
     return values[port_id];
 }
 
-void test_write() {
+void test_rw() {
     m6522_t via;
     m6522_init(&via, in_cb, out_cb);
 
+    /* write DDR and OUT value of port B */
+    uint64_t pins = M6522_CS1;
+    M6522_SET_ADDR(pins, M6522_REG_DDRB);
+    M6522_SET_DATA(pins, 0xAA);
+    pins = m6522_iorq(&via, pins);
+    T(via.ddr_b == 0xAA);
+    T(values[M6522_PORT_B] == 0x55);
+    M6522_SET_ADDR(pins, M6522_REG_RB);
+    M6522_SET_DATA(pins, 0xF0);
+    pins = m6522_iorq(&via, pins);
+    T(via.out_b = 0xF0);
+    T(values[M6522_PORT_B] == 0xF5);
 
-}
-
-void test_read() {
-
+    /* read back B values */
+    pins = M6522_CS1|M6522_RW;
+    M6522_SET_ADDR(pins, M6522_REG_DDRB);
+    pins = m6522_iorq(&via, pins);
+    T(M6522_GET_DATA(pins) == 0xAA);
+    M6522_SET_ADDR(pins, M6522_REG_RB);
+    pins = m6522_iorq(&via, pins);
+    T(M6522_GET_DATA(pins) == 0xF5);
 }
 
 int main() {
-    test_write();
-    test_read();
+    test_rw();
     return 0;
 }
