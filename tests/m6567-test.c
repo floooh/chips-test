9c18fe7d93cf19216fd241df42ec25b6dae507e2
Andre Weissflog <floooh@gmail.com>
2018-02-25 23:00:07 +0100

    Started with m6567 (aka VIC-II)

---
+++ tests/m6567-test.c
@@ -0,0 +1,39 @@
+//------------------------------------------------------------------------------
+//  m6567-test.c
+//------------------------------------------------------------------------------
+// force assert() enabled
+#ifdef NDEBUG
+#undef NDEBUG
+#endif
+#define CHIPS_IMPL
+#include "chips/m6567.h"
+#include <stdio.h>
+
+uint32_t num_tests = 0;
+#define T(x) { assert(x); num_tests++; }
+
+uint32_t rgba8_buffer[262144];
+
+uint64_t fetch(uint64_t pins) {
+    // FIXME
+    return pins;
+}
+
+void test_rw() {
+    m6567_t vic;
+    m6567_init(&vic, &(m6567_desc_t){
+        .type = M6567_TYPE_6569,
+        .rgba8_buffer = rgba8_buffer,
+        .rgba8_buffer_size = sizeof(rgba8_buffer),
+        .fetch_cb = fetch
+    });
+    int w, h;
+    m6567_display_size(&vic, &w, &h);
+    T(w == 403);
+    T(h == 284);
+}
+
+int main() {
+    test_rw();
+    return 0;
+}
