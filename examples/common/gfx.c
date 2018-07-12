8dad04e45c75bb98f5b2ccb2fe325aee7584234f
Andre Weissflog <floooh@gmail.com>
2018-07-12 12:31:50 +0200

    use a separate upscale pass in the emulator examples

--- examples/common/gfx.c
+++ examples/common/gfx.c
@@ -6,9 +6,14 @@
 extern const char* vs_src; 
 extern const char* fs_src;
 
-static const sg_pass_action pass_action = {
+static const sg_pass_action upscale_pass_action = {
+    .colors[0] = { .action = SG_ACTION_DONTCARE }
+};
+static const sg_pass_action draw_pass_action = {
     .colors[0] = { .action = SG_ACTION_CLEAR, .val = { 0.25f, 0.25f, 0.25f, 1.0f } }
 };
+static sg_draw_state upscale_draw_state;
+static sg_pass upscale_pass;
 static sg_draw_state draw_state;
 static int fb_width;
 static int fb_height;
@@ -33,11 +38,29 @@ void gfx_init(int w, int h, int sx, int sy) {
     });
     stm_setup();
 
-    float quad_vertices[] = { 0.0f, 0.0f,  1.0f, 0.0f,  0.0f, 1.0f,  1.0f, 1.0f };
-    draw_state.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
-        .size = sizeof(quad_vertices),
-        .content = quad_vertices,
+    /* quad vertex buffers with and without flipped UVs */
+    float verts[] = {
+        0.0f, 0.0f, 0.0f, 0.0f,
+        1.0f, 0.0f, 1.0f, 0.0f,
+        0.0f, 1.0f, 0.0f, 1.0f,
+        1.0f, 1.0f,  1.0f, 1.0f
+    };
+    float verts_flipped[] = {
+        0.0f, 0.0f, 0.0f, 1.0f,
+        1.0f, 0.0f, 1.0f, 1.0f,
+        0.0f, 1.0f, 0.0f, 0.0f,
+        1.0f, 1.0f,  1.0f, 0.0f
+    };
+    upscale_draw_state.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(verts),
+        .content = verts,
     });
+    draw_state.vertex_buffers[0] = sg_make_buffer(&(sg_buffer_desc){
+        .size = sizeof(verts),
+        .content = sg_query_feature(SG_FEATURE_ORIGIN_TOP_LEFT) ? verts : verts_flipped
+    });
+
+    /* a shader to render a textured quad */
     sg_shader fsq_shd = sg_make_shader(&(sg_shader_desc){
         .fs.images = {
             [0] = { .name="tex", .type=SG_IMAGETYPE_2D },
@@ -45,25 +68,53 @@ void gfx_init(int w, int h, int sx, int sy) {
         .vs.source = vs_src,
         .fs.source = fs_src,
     });
-    draw_state.pipeline = sg_make_pipeline(&(sg_pipeline_desc){
+
+    /* 2 pipeline-state-objects, one for upscaling, one for rendering */
+    sg_pipeline_desc pip_desc = {
         .layout = {
-            .attrs[0] = { .name="pos", .sem_name="POSITION", .format=SG_VERTEXFORMAT_FLOAT2 }
+            .attrs[0] = { .name="in_pos", .sem_name="POS", .format=SG_VERTEXFORMAT_FLOAT2 },
+            .attrs[1] = { .name="in_uv", .sem_name="UV", .format=SG_VERTEXFORMAT_FLOAT2 }
         },
         .shader = fsq_shd,
         .primitive_type = SG_PRIMITIVETYPE_TRIANGLE_STRIP
-    });
-    draw_state.fs_images[0] = sg_make_image(&(sg_image_desc){
+    };
+    draw_state.pipeline = sg_make_pipeline(&pip_desc);
+    pip_desc.blend.depth_format = SG_PIXELFORMAT_NONE;
+    upscale_draw_state.pipeline = sg_make_pipeline(&pip_desc);
+
+    /* a texture with the emulator's raw pixel data */
+    upscale_draw_state.fs_images[0] = sg_make_image(&(sg_image_desc){
         .width = fb_width,
         .height = fb_height,
         .pixel_format = SG_PIXELFORMAT_RGBA8,
         .usage = SG_USAGE_STREAM,
+        .min_filter = SG_FILTER_NEAREST,
+        .mag_filter = SG_FILTER_NEAREST,
+        .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
+        .wrap_v = SG_WRAP_CLAMP_TO_EDGE
+    });
+    /* a 2x upscaled render-target-texture */
+    draw_state.fs_images[0] = sg_make_image(&(sg_image_desc){
+        .render_target = true,
+        .width = 2*fb_width,
+        .height = 2*fb_height,
+        .pixel_format = SG_PIXELFORMAT_RGBA8,
         .min_filter = SG_FILTER_LINEAR,
         .mag_filter = SG_FILTER_LINEAR,
         .wrap_u = SG_WRAP_CLAMP_TO_EDGE,
         .wrap_v = SG_WRAP_CLAMP_TO_EDGE
     });
+
+    /* a render pass for the 2x upscaling */
+    upscale_pass = sg_make_pass(&(sg_pass_desc){
+        .color_attachments[0].image = draw_state.fs_images[0]
+    });
 }
 
+/* apply a viewport rectangle to preserve the emulator's aspect ratio,
+   and for 'portrait' orientations, keep the emulator display at the
+   top, to make room at the bottom for mobile virtual keyboard
+*/
 static void apply_viewport(void) {
     const int canvas_width = sapp_width();
     const int canvas_height = sapp_height();
@@ -88,13 +139,22 @@ static void apply_viewport(void) {
 }
 
 void gfx_draw() {
-    sg_update_image(draw_state.fs_images[0], &(sg_image_content){
+    /* copy emulator pixel data into upscaling source texture */
+    sg_update_image(upscale_draw_state.fs_images[0], &(sg_image_content){
         .subimage[0][0] = { 
             .ptr = rgba8_buffer,
             .size = fb_width*fb_height*sizeof(uint32_t)
         }
     });
-    sg_begin_default_pass(&pass_action, sapp_width(), sapp_height());
+
+    /* upscale the original framebuffer 2x with nearest filtering */
+    sg_begin_pass(upscale_pass, &upscale_pass_action);
+    sg_apply_draw_state(&upscale_draw_state);
+    sg_draw(0, 4, 1);
+    sg_end_pass();
+
+    /* draw the final pass with linear filtering */
+    sg_begin_default_pass(&draw_pass_action, sapp_width(), sapp_height());
     apply_viewport();
     sg_apply_draw_state(&draw_state);
     sg_draw(0, 4, 1);
@@ -110,11 +170,12 @@ void gfx_shutdown() {
 #if defined(SOKOL_GLCORE33)
 const char* vs_src =
     "#version 330\n"
-    "in vec2 pos;\n"
+    "in vec2 in_pos;\n"
+    "in vec2 in_uv;\n"
     "out vec2 uv;\n"
     "void main() {\n"
-    "  gl_Position = vec4(pos*2.0-1.0, 0.5, 1.0);\n"
-    "  uv = vec2(pos.x, 1.0-pos.y);\n"
+    "  gl_Position = vec4(in_pos*2.0-1.0, 0.5, 1.0);\n"
+    "  uv = in_uv;\n"
     "}\n";
 const char* fs_src =
     "#version 330\n"
@@ -126,11 +187,12 @@ const char* fs_src =
     "}\n";
 #elif defined(SOKOL_GLES2)
 const char* vs_src =
-    "attribute vec2 pos;\n"
+    "attribute vec2 in_pos;\n"
+    "attribute vec2 in_uv;\n"
     "varying vec2 uv;\n"
     "void main() {\n"
-    "  gl_Position = vec4(pos*2.0-1.0, 0.5, 1.0);\n"
-    "  uv = vec2(pos.x, 1.0-pos.y);\n"
+    "  gl_Position = vec4(in_pos*2.0-1.0, 0.5, 1.0);\n"
+    "  uv = in_uv;\n"
     "}\n";
 const char* fs_src =
     "precision mediump float;\n"
@@ -145,6 +207,7 @@ const char* vs_src =
     "using namespace metal;\n"
     "struct vs_in {\n"
     "  float2 pos [[attribute(0)]];\n"
+    "  float2 uv [[attribute(1)]];\n"
     "};\n"
     "struct vs_out {\n"
     "  float4 pos [[position]];\n"
@@ -153,7 +216,7 @@ const char* vs_src =
     "vertex vs_out _main(vs_in in [[stage_in]]) {\n"
     "  vs_out out;\n"
     "  out.pos = float4(in.pos*2.0-1.0, 0.5, 1.0);\n"
-    "  out.uv = float2(in.pos.x, 1.0-in.pos.y);\n"
+    "  out.uv = in.uv;\n"
     "  return out;\n"
     "}\n";
 const char* fs_src =
@@ -168,7 +231,8 @@ const char* fs_src =
 #elif defined(SOKOL_D3D11)
 const char* vs_src =
     "struct vs_in {\n"
-    "  float2 pos: POSITION;\n"
+    "  float2 pos: POS;\n"
+    "  float2 uv:  UV;\n"
     "};\n"
     "struct vs_out {\n"
     "  float2 uv: TEXCOORD0;\n"
@@ -177,7 +241,7 @@ const char* vs_src =
     "vs_out main(vs_in inp) {\n"
     "  vs_out outp;\n"
     "  outp.pos = float4(inp.pos*2.0-1.0, 0.5, 1.0);\n"
-    "  outp.uv = float2(inp.pos.x, 1.0-inp.pos.y);\n"
+    "  outp.uv = in.uv;\n"
     "  return outp;\n"
     "}\n";
 const char* fs_src =
