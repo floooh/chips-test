bb4d5432f985aaa73095ddb065e9380407790fd3
Andre Weissflog <floooh@gmail.com>
2018-07-18 13:29:01 +0200

    Added CPC games and demos to example webpage

--- fips-files/verbs/webpage.py
+++ fips-files/verbs/webpage.py
@@ -9,13 +9,30 @@ from string import Template
 
 from mod import log, util, project
 
-samples = [
-    [ 'ZX Spectrum 128', 'zx128k', ''],
-    [ 'Commodore C64', 'c64', ''],
-    [ 'Amstrad CPC6128', 'cpc6128', ''],
-    [ 'Acorn Atom', 'atom', ''],
-    [ 'Robotron Z1013', 'z1013', 'J 300[Enter] => BASIC'],
-    [ 'Robotron KC87', 'kc87', 'BASIC[Enter]'],
+systems = [ 'zx128k', 'c64', 'cpc6128', 'atom', 'z1013', 'kc87' ]
+items = [
+    { 'type':'emu',  'title':'ZX Spectrum 128', 'system':'zx128k',     'url':'zx128k.html',  'img':'zx128k/zx128k.jpg', 'note':'' },
+    { 'type':'emu',  'title':'Commodore C64',   'system':'c64',        'url':'c64.html',     'img':'c64/c64.jpg',       'note':'' },
+    { 'type':'emu',  'title':'Amstrad CPC6128', 'system':'cpc6128',    'url':'cpc6128.html', 'img':'cpc6128/cpc6128.jpg',   'note':'' },
+    { 'type':'emu',  'title':'Acorn Atom',      'system':'atom',       'url':'atom.html',    'img':'atom/atom.jpg',     'note':'' },
+    { 'type':'emu',  'title':'Robotron Z1013',  'system':'z1013',      'url':'z1013.html',   'img':'z1013/z1013.jpg',   'note':'J 300[Enter] => BASIC' },
+    { 'type':'emu',  'title':'Robotron KC87',   'system':'kc87',       'url':'kc87.html',    'img':'kc87/kc87.jpg',     'note':'BASIC[Enter]' },
+    { 'type':'demo', 'title':'DTC',                 'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/dtc.sna', 'img':'cpc6128/dtc.jpg', 'note':'by Overlanders (CPC)' },
+    { 'type':'demo', 'title':'Ecole Buissonniere',  'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/ecole_buissonniere.sna', 'img':'cpc6128/ecole_buissonniere.jpg', 'note':'by MadRam/OVL (CPC)'},
+    { 'type':'game', 'title':'Boulderdash (CPC)',   'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/boulder_dash.sna', 'img':'cpc6128/boulderdash_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':'Bomb Jack (CPC)',     'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/bomb_jack.sna', 'img':'cpc6128/bombjack_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':'Cybernoid (CPC)',     'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/cybernoid.sna', 'img':'cpc6128/cybernoid_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':'Arkanoid (CPC)',      'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/arkanoid.sna', 'img':'cpc6128/arkanoid_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':'Chase HQ (CPC)',      'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/chase_hq.sna', 'img':'cpc6128/chasehq_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':"Ghosts'n'Goblins (CPC)", 'system':'cpc6128',  'url':'cpc6128.html?file=cpc6128/ghosts_n_goblins.sna', 'img':'cpc6128/ghostsngoblins_cpc.jpg', 'note':''},
+#    { 'type':'game', 'title':"Fruity Frank (CPC)",  'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/fruity_frank.sna', 'img':'cpc6128/fruityfrank_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':"1943 (CPC)",          'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/1943.sna', 'img':'cpc6128/1943_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':"Dragon Ninja (CPC)",  'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/dragon_ninja.sna', 'img':'cpc6128/dragonninja_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':"Gryzor (CPC)",        'system':'cpc6128',     'url':'cpc6128.html?file=cpc6128/gryzor.sna', 'img':'cpc6128/gryzor_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':"Ikari Warriors (CPC)", 'system':'cpc6128',    'url':'cpc6128.html?file=cpc6128/ikari_warriors.sna', 'img':'cpc6128/ikariwarriors_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':"Head over Heels (CPC)", 'system':'cpc6128',   'url':'cpc6128.html?file=cpc6128/head_over_heels.sna', 'img':'cpc6128/headoverheels_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':"Rick Dangerous (CPC)",  'system':'cpc6128',   'url':'cpc6128.html?file=cpc6128/rick_dangerous.sna', 'img':'cpc6128/rickdangerous_cpc.jpg', 'note':''},
+    { 'type':'game', 'title':"Donkey Kong (CPC)",     'system':'cpc6128',   'url':'cpc6128.html?file=cpc6128/donkey_kong.sna', 'img':'cpc6128/donkeykong_cpc.jpg', 'note':''},
 ]
 GitHubSamplesURL = 'https://github.com/floooh/chips-test/master/examples'
 BuildConfig = 'wasm-ninja-release'
@@ -27,19 +44,16 @@ def deploy_webpage(fips_dir, proj_dir, webpage_dir) :
 
     # build the thumbnail gallery
     content = ''
-    for sample in samples :
-        display_name = sample[0]
-        name = sample[1]
-        note = sample[2]
-        log.info('> adding thumbnail for {}'.format(name))
-        img_name = name + '.jpg'
-        img_path = proj_dir + '/webpage/' + img_name
-        if not os.path.exists(img_path):
-            img_name = 'dummy.jpg'
-            img_path = proj_dir + 'webpage/dummy.jpg'
+    for item in items :
+        title = item['title']
+        system = item['system']
+        url = item['url']
+        image = item['img']
+        note = item['note']
+        log.info('> adding thumbnail for {}'.format(url))
         content += '<div class="thumb">\n'
-        content += '  <div class="thumb-title">{}</div>\n'.format(display_name)
-        content += '  <div class="img-frame"><a href="{}.html"><img class="image" src="{}"></img></a></div>\n'.format(name,img_name)
+        content += '  <div class="thumb-title">{}</div>\n'.format(title)
+        content += '  <div class="img-frame"><a href="{}"><img class="image" src="{}"></img></a></div>\n'.format(url,image)
         content += '  <div class="thumb-bar">{}</div>\n'.format(note)
         content += '</div>\n'
 
@@ -62,11 +76,13 @@ def deploy_webpage(fips_dir, proj_dir, webpage_dir) :
         log.info('> copy file: {}'.format(name))
         shutil.copy(proj_dir + '/webpage/' + name, webpage_dir + '/' + name)
 
-    # generate sample HTML pages
+    # generate emu HTML pages
     emsc_deploy_dir = '{}/fips-deploy/chips-test/{}'.format(ws_dir, BuildConfig)
-    for sample in samples :
-        display_name = sample[0]
-        name = sample[1]
+    for item in items :
+        if item['type'] != 'emu':
+            continue
+        name = os.path.splitext(item['url'])[0]
+        title = item['title']
         log.info('> generate emscripten HTML page: {}'.format(name))
         for ext in ['wasm', 'js'] :
             src_path = '{}/{}.{}'.format(emsc_deploy_dir, name, ext)
@@ -75,23 +91,15 @@ def deploy_webpage(fips_dir, proj_dir, webpage_dir) :
         with open(proj_dir + '/webpage/emsc.html', 'r') as f :
             templ = Template(f.read())
         src_url = GitHubSamplesURL + name
-        html = templ.safe_substitute(name=display_name, prog=name, source=src_url)
-        with open('{}/{}.html'.format(webpage_dir, name, name), 'w') as f :
+        html = templ.safe_substitute(name=title, prog=name, source=src_url)
+        with open('{}/{}.html'.format(webpage_dir, name), 'w') as f :
             f.write(html)
 
-    # copy the screenshots
-    for sample in samples :
-        img_name = sample[1] + '.jpg'
-        img_path = proj_dir + '/webpage/' + img_name
-        if os.path.exists(img_path):
-            log.info('> copy screenshot: {}'.format(img_name))
-            shutil.copy(img_path, webpage_dir + '/' + img_name)
-
-    # copy the game files
-    for sample in samples:
-        src_dir = proj_dir + '/webpage/' + sample[1]
+    # copy data files and images
+    for system in systems:
+        src_dir = proj_dir + '/webpage/' + system
         if os.path.exists(src_dir):
-            dst_dir = webpage_dir + '/' + sample[1]
+            dst_dir = webpage_dir + '/' + system
             if os.path.isdir(dst_dir):
                 shutil.rmtree(dst_dir)
             shutil.copytree(src_dir, dst_dir)
