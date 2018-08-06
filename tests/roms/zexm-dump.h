21eea484ad147f2d933236a7d4742052964585f3
Andre Weissflog <floooh@gmail.com>
2018-08-06 19:36:41 +0200

    add ZEXDOC/ZEXALL test for z80m.h

---
+++ tests/roms/zexm-dump.h
@@ -0,0 +1,9 @@
+#pragma once
+// #version:5#
+// machine generated, do not edit!
+#include <stdint.h>
+extern unsigned char dump_zexall[8585];
+extern unsigned char dump_zexdoc[8585];
+typedef struct { const char* name; const uint8_t* ptr; int size; } dump_item;
+#define DUMP_NUM_ITEMS (2)
+extern dump_item dump_items[DUMP_NUM_ITEMS];
