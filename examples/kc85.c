851bf136a23d7daeaaae66c6bb45ea8b110f7685
Andre Weissflog <floooh@gmail.com>
2018-09-09 17:55:48 +0200

    kc85-curses improvements

--- examples/kc85.c
+++ examples/kc85.c
@@ -20,15 +20,30 @@
 kc85_t kc85;
 uint32_t fb[KC85_DISPLAY_WIDTH * KC85_DISPLAY_HEIGHT];
 
+/* refresh at 30fps */
+#define FRAME_USEC (33333)
+
+/* convert a KC color to ncurses color. KC85 has 16 foreground colors, ncurses
+only 8, so we duplicate the foreground colors
+*/
+int kc85_to_curses_color(int c) {
+    switch (c & 7) {
+        case 0: return COLOR_BLACK;
+        case 1: return COLOR_BLUE;
+        case 2: return COLOR_RED;
+        case 3: return COLOR_MAGENTA;
+        case 4: return COLOR_GREEN;
+        case 5: return COLOR_CYAN;
+        case 6: return COLOR_YELLOW;
+        default: return COLOR_WHITE;
+    }
+}
+
 int main() {
     kc85_init(&kc85, &(kc85_desc_t){
         .type = KC85_TYPE_4,
         .pixel_buffer = fb,
         .pixel_buffer_size = sizeof(fb),
-        .rom_caos22 = dump_caos22,
-        .rom_caos22_size = sizeof(dump_caos22),
-        .rom_caos31 = dump_caos31,
-        .rom_caos31_size = sizeof(dump_caos31),
         .rom_caos42c = dump_caos42c,
         .rom_caos42c_size = sizeof(dump_caos42c),
         .rom_caos42e = dump_caos42e,
@@ -43,9 +58,33 @@ int main() {
     cbreak();
     nodelay(stdscr, TRUE);
     keypad(stdscr, TRUE);
+
+    /* setup color pairs, KC85 has 16 foreground and 8 background colors, 
+       ncurses has only 8 colos
+    */
+    start_color();
+    init_color(COLOR_BLACK, 0, 0, 0);
+    init_color(COLOR_BLUE, 0, 0, 1000);
+    init_color(COLOR_RED, 1000, 0, 0);
+    init_color(COLOR_MAGENTA, 1000, 0, 1000);
+    init_color(COLOR_GREEN, 0, 1000, 0);
+    init_color(COLOR_CYAN, 0, 1000, 1000);
+    init_color(COLOR_YELLOW, 1000, 1000, 0);
+    init_color(COLOR_WHITE, 1000, 1000, 1000);
+    for (int fg = 0; fg < 16; fg++) {
+        for (int bg = 0; bg < 8; bg++) {
+            int curses_fg = kc85_to_curses_color(fg);
+            int curses_bg = kc85_to_curses_color(bg);
+            int index = fg*8 + bg;
+            assert(index < COLOR_PAIRS);
+            init_pair(index, curses_fg, curses_bg);
+        }
+    }
     while (true) {
-        kc85_exec(&kc85, 66667);
-        clear();
+        /* tick the emulator */
+        kc85_exec(&kc85, FRAME_USEC);
+
+        /* keyboard input */
         int ch = getch();
         if (ch != ERR) {
             switch (ch) {
@@ -69,18 +108,41 @@ int main() {
                 kc85_key_up(&kc85, ch);
             }
         }
+
+        /* render the display */
+        int cursor_x = kc85.ram[4][0x37A0];
+        int cursor_y = kc85.ram[4][0x37A1];
+        int cur_color_byte = -1;
         for (int y = 0; y < 32; y++) {
             for (int x = 0; x < 40; x++) {
-                const char* ptr = (const char*) &kc85.ram[4][0x3200 + y*40 + x];
-                char chr = *ptr;
+                /* get color code from color buffer, not the color buffer is 90 degree rotated! */
+                int color_byte = kc85.ram[5][x*256 + y*8] & 0x7f;
+                if (color_byte != cur_color_byte) {
+                    attron(COLOR_PAIR(color_byte));
+                }
+                /* padding to get 4x3 aspect ratio */
+                mvaddch(y, x*2, ' ');
+                /* get character code from ASCII buffer */
+                char chr = (char) kc85.ram[4][0x3200 + y*40 + x];
                 if ((chr < 20) || (chr > 127)) {
                     chr = 0x20;
                 }
-                mvaddch(y, x, chr);
+                /* cursor on? */
+                if ((x == cursor_x) && (y == cursor_y)) {
+                    attrset(A_REVERSE);
+                }
+                /* render character */
+                mvaddch(y, x*2+1, chr);
+                /* cursor off? */
+                if ((x == cursor_x) && (y == cursor_y)) {
+                    attrset(A_NORMAL);
+                }
             }
         }
         refresh();
-        usleep(66667);
+
+        /* wait for next frame */
+        usleep(FRAME_USEC);
     }
     endwin();
     return 0;
