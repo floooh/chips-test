a169b9701fdc84bcfee418febc3efbad69e02066
Andre Weissflog <floooh@gmail.com>
2018-09-28 21:09:32 +0200

    fix a comment bug

--- examples/sokol/bombjack.c
+++ examples/sokol/bombjack.c
@@ -410,7 +410,7 @@ uint8_t bombjack_ay_in(int port_id, void* user_data) {
 
     The 3 bitmaps for each tile are 8 KBytes apart (basically each
     of the 3 background-tile ROM chips contains one set of bitmaps
-    for all 512 tiles).
+    for all 256 tiles).
 
     The 3 bitmaps are combined to get the lower 3 bits of the
     color palette index. The remaining 4 bits of the palette
