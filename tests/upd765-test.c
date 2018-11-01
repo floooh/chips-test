a46cc53898932cc98e58e852caca6d736f6df984
Andre Weissflog <floooh@gmail.com>
2018-11-01 21:02:33 +0100

    more upd765.h tests

--- tests/upd765-test.c
+++ tests/upd765-test.c
@@ -25,15 +25,28 @@ uint8_t rd(void) {
     return UPD765_GET_DATA(pins);
 }
 
+bool info(int drive, int side, int track, void* user_data, upd765_track_info_t* out_info) {
+    // FIXME
+    out_info->side = side;
+    out_info->track = track;
+    out_info->sector_id = 0xC1;
+    out_info->sector_size = 2;
+    out_info->st1 = 0;
+    out_info->st2 = 0;
+    return true;
+}
+
 void tick(void) {
     upd765_tick(&upd);
 }
 
 void init() {
-    upd765_init(&upd, &(upd765_desc_t){0});
-        T(UPD765_PHASE_IDLE == upd.phase);
-        T(0 == upd.fifo_pos);
-        T(UPD765_STATUS_RQM == status());
+    upd765_init(&upd, &(upd765_desc_t){
+        .info_cb = info
+    });
+    T(UPD765_PHASE_IDLE == upd.phase);
+    T(0 == upd.fifo_pos);
+    T(UPD765_STATUS_RQM == status());
 }
 
 void test_init(void) {
@@ -110,7 +123,16 @@ void do_sense_interrupt_status(void) {
 
 void do_read_id(void) {
     wr(0x0A);
-
+        T(UPD765_PHASE_COMMAND == upd.phase);
+        T((UPD765_STATUS_RQM|UPD765_STATUS_CB) == status());
+        T(0x0A == upd.fifo[0]);
+        T(1 == upd.fifo_pos);
+        T(2 == upd.fifo_num);
+    wr(0x00);
+        T(UPD765_PHASE_RESULT == upd.phase);
+        T((UPD765_STATUS_CB|UPD765_STATUS_EXM|UPD765_STATUS_RQM) == status());
+        T((UPD765_STATUS_RQM|UPD765_STATUS_CB|UPD765_STATUS_DIO) == status());
+        T(7 == upd.fifo_num);
 }
 
 void test_recalibrate(void) {
