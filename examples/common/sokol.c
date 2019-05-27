8274ee1edfaee82ab000d702cc460545500496fb
Andre Weissflog <floooh@gmail.com>
2019-05-27 16:17:36 +0200

    remove SOKOL_D3D11_SHADER_COMPILER define

--- examples/common/sokol.c
+++ examples/common/sokol.c
@@ -3,7 +3,6 @@ on MacOS and iOS the implementation must be compiled as Objective-C, so there
 must be a *.m file on MacOS/iOS, and *.c file everywhere else
 */
 #define SOKOL_IMPL
-#define SOKOL_D3D11_SHADER_COMPILER
 #if defined(_WIN32)
 #include <Windows.h>
 #define SOKOL_LOG(s) OutputDebugStringA(s)
