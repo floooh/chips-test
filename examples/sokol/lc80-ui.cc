6a08911007e84f03154e633cf9d915ecf4eda95f
Andre Weissflog <floooh@gmail.com>
2019-09-14 13:27:26 +0200

    start with LC-80 example

---
+++ examples/sokol/lc80-ui.cc
@@ -0,0 +1,91 @@
+/*
+    lc80-ui.cc
+*/
+#include "common.h"
+#include "imgui.h"
+#include "chips/z80.h"
+#include "chips/z80ctc.h"
+#include "chips/z80pio.h"
+#include "chips/beeper.h"
+#include "chips/clk.h"
+#include "chips/kbd.h"
+#include "chips/mem.h"
+#include "systems/lc80.h"
+#define CHIPS_IMPL
+#define UI_DASM_USE_Z80
+#define UI_DBG_USE_Z80
+#include "ui.h"
+#include "util/z80dasm.h"
+#include "ui/ui_util.h"
+#include "ui/ui_chip.h"
+#include "ui/ui_memedit.h"
+#include "ui/ui_dasm.h"
+#include "ui/ui_dbg.h"
+#include "ui/ui_audio.h"
+#include "ui/ui_kbd.h"
+#include "ui/ui_z80.h"
+#include "ui/ui_z80pio.h"
+#include "ui/ui_z80ctc.h"
+#include "ui/ui_lc80.h"
+#include "ui/ui_audio.h"
+
+#ifdef __clang__
+#pragma clang diagnostic ignored "-Wmissing-field-initializers"
+#endif
+
+extern "C" {
+
+extern lc80_desc_t lc80_desc(void);
+
+static double exec_time;
+static ui_lc80_t ui_lc80;
+
+/* reboot callback */
+static void boot_cb(lc80_t* sys) {
+    lc80_desc_t desc = lc80_desc();
+    lc80_init(ui_lc80.sys, &desc);
+}
+
+void lc80ui_draw(void) {
+    ui_lc80_draw(&ui_lc80, exec_time);
+}
+
+void lc80ui_init(lc80_t* sys) {
+    ui_init(lc80ui_draw);
+    ui_lc80_desc_t desc = { 0 };
+    desc.sys = sys;
+    desc.boot_cb = boot_cb;
+    desc.create_texture_cb = gfx_create_texture;
+    desc.update_texture_cb = gfx_update_texture;
+    desc.destroy_texture_cb = gfx_destroy_texture;
+    desc.dbg_keys.break_keycode = SAPP_KEYCODE_F5;
+    desc.dbg_keys.break_name = "F5";
+    desc.dbg_keys.continue_keycode = SAPP_KEYCODE_F5;
+    desc.dbg_keys.continue_name = "F5";
+    desc.dbg_keys.step_over_keycode = SAPP_KEYCODE_F6;
+    desc.dbg_keys.step_over_name = "F6";
+    desc.dbg_keys.step_into_keycode = SAPP_KEYCODE_F7;
+    desc.dbg_keys.step_into_name = "F7";
+    desc.dbg_keys.toggle_breakpoint_keycode = SAPP_KEYCODE_F9;
+    desc.dbg_keys.toggle_breakpoint_name = "F9";
+    ui_lc80_init(&ui_lc80, &desc);
+}
+
+void lc80ui_discard(void) {
+    ui_lc80_discard(&ui_lc80);
+}
+
+void lc80ui_exec(uint32_t frame_time_us) {
+    if (ui_lc80_before_exec(&ui_lc80)) {
+        uint64_t start = stm_now();
+        lc80_exec(ui_lc80.sys, frame_time_us);
+        exec_time = stm_ms(stm_since(start));
+        ui_lc80_after_exec(&ui_lc80);
+    }
+    else {
+        exec_time = 0.0;
+    }
+}
+
+} /* extern "C" */
+
---
+++ examples/sokol/lc80.c
@@ -0,0 +1,100 @@
+/*
+    lc80.c
+*/
+#include "sokol_app.h"
+#include "sokol_gfx.h"
+#include "sokol_args.h"
+#include "sokol_audio.h"
+#include "clock.h"
+#define CHIPS_IMPL
+#include "chips/z80.h"
+#include "chips/z80ctc.h"
+#include "chips/z80pio.h"
+#include "chips/beeper.h"
+#include "chips/clk.h"
+#include "chips/kbd.h"
+#include "chips/mem.h"
+#include "systems/lc80.h"
+#include "lc80-roms.h"
+
+/* imports from lc80-ui.cc */
+#include "ui.h"
+void lc80ui_init(lc80_t* lc80);
+void lc80ui_discard(void);
+void lc80ui_draw(void);
+void lc80ui_exec(uint32_t frame_time_us);
+
+static lc80_t sys;
+
+/* sokol-app entry, configure application callbacks and window */
+static void app_init(void);
+static void app_frame(void);
+static void app_input(const sapp_event*);
+static void app_cleanup(void);
+
+sapp_desc sokol_main(int argc, char* argv[]) {
+    sargs_setup(&(sargs_desc) { .argc = argc, .argv = argv });
+    return (sapp_desc) {
+        .init_cb = app_init,
+        .frame_cb = app_frame,
+        .event_cb = app_input,
+        .cleanup_cb = app_cleanup,
+        .width = 1024,
+        .height = 720,
+        .window_title = "LC-80",
+        .ios_keyboard_resizes_canvas = true
+    };
+}
+
+/* audio-streaming callback */
+static void push_audio(const float* samples, int num_samples, void* user_data) {
+    saudio_push(samples, num_samples);
+}
+
+lc80_desc_t lc80_desc(void) {
+    return (lc80_desc_t) {
+        .audio_cb = push_audio,
+        .audio_sample_rate = saudio_sample_rate(),
+        .rom_ptr = dump_lc80_2k_bin,
+        .rom_size = sizeof(dump_lc80_2k_bin)
+    };
+}
+
+void app_init(void) {
+    sg_setup(&(sg_desc){
+        .mtl_device = sapp_metal_get_device(),
+        .mtl_renderpass_descriptor_cb = sapp_metal_get_renderpass_descriptor,
+        .mtl_drawable_cb = sapp_metal_get_drawable,
+        .d3d11_device = sapp_d3d11_get_device(),
+        .d3d11_device_context = sapp_d3d11_get_device_context(),
+        .d3d11_render_target_view_cb = sapp_d3d11_get_render_target_view,
+        .d3d11_depth_stencil_view_cb = sapp_d3d11_get_depth_stencil_view
+    });
+    clock_init();
+    saudio_setup(&(saudio_desc){0});
+
+    lc80_desc_t desc = lc80_desc();
+    lc80_init(&sys, &desc);
+}
+
+void app_frame(void) {
+    lc80ui_exec(clock_frame_time());
+    lc80ui_draw();
+}
+
+void app_input(const sapp_event* event) {
+    if (ui_input(event)) {
+        return;
+    }
+    else {
+        // FIXME: forward input to emulator
+    }
+}
+
+void app_cleanup(void) {
+    lc80_discard(&sys);
+    lc80ui_discard();
+    saudio_shutdown();
+    sg_shutdown();
+    sargs_shutdown();
+}
