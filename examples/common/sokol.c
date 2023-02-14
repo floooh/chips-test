cec94e468fa26ace528c2dda684d166681d9791a
Andre Weissflog <floooh@gmail.com>
2023-02-14 19:06:27 +0100

    remove SOKOL_LOG define, no longer needed

--- examples/common/sokol.c
+++ examples/common/sokol.c
@@ -3,9 +3,6 @@ on MacOS and iOS the implementation must be compiled as Objective-C, so there
 must be a *.m file on MacOS/iOS, and *.c file everywhere else
 */
 #define SOKOL_IMPL
-#if defined(_WIN32)
-#define SOKOL_LOG(s) OutputDebugStringA(s)
-#endif
 /* sokol 3D-API defines are provided by build options */
 #include "sokol_app.h"
 #include "sokol_gfx.h"
