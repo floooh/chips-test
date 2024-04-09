e11477a6434e5aa5a95754d3bfcdbc1ef3266c93
scemino <scemino74@gmail.com>
2024-04-09 21:15:28 +0200

    nes: use ui methods

--- examples/sokol/nes.c
+++ examples/sokol/nes.c
@@ -84,15 +84,15 @@ static void app_init(void) {
     ui_nes_init(&state.ui, &(ui_nes_desc_t){
         .nes = &state.nes,
         .dbg_texture = {
-            .create_cb = gfx_create_texture,
-            .update_cb = gfx_update_texture,
-            .destroy_cb = gfx_destroy_texture,
+            .create_cb = ui_create_texture,
+            .update_cb = ui_update_texture,
+            .destroy_cb = ui_destroy_texture,
         },
         .snapshot = {
             .load_cb = ui_load_snapshot,
             .save_cb = ui_save_snapshot,
             .empty_slot_screenshot = {
-                .texture = gfx_shared_empty_snapshot_texture(),
+                .texture = ui_shared_empty_snapshot_texture(),
             }
         },
         .dbg_keys = {
@@ -251,11 +251,11 @@ static void ui_draw_cb(void) {
 
 static void ui_update_snapshot_screenshot(size_t slot) {
     ui_snapshot_screenshot_t screenshot = {
-        .texture = gfx_create_screenshot_texture(nes_display_info(&state.snapshots[slot].nes))
+        .texture = ui_create_screenshot_texture(nes_display_info(&state.snapshots[slot].nes))
     };
     ui_snapshot_screenshot_t prev_screenshot = ui_snapshot_set_screenshot(&state.ui.snapshot, slot, screenshot);
     if (prev_screenshot.texture) {
-        gfx_destroy_texture(prev_screenshot.texture);
+        ui_destroy_texture(prev_screenshot.texture);
     }
 }
 
