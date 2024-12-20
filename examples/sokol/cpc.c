03d3d69546259b5c4fcc0d0a10af75436334432d
Andre Weissflog <floooh@gmail.com>
2024-12-20 14:23:59 +0100

    ui: add load/save settings to cpc

--- examples/sokol/cpc.c
+++ examples/sokol/cpc.c
@@ -22,6 +22,7 @@
 #if defined(CHIPS_USE_UI)
     #define UI_DBG_USE_Z80
     #include "ui.h"
+    #include "ui/ui_settings.h"
     #include "ui/ui_chip.h"
     #include "ui/ui_memedit.h"
     #include "ui/ui_memmap.h"
@@ -65,6 +66,7 @@ static struct {
 #ifdef CHIPS_USE_UI
 #define BORDER_TOP (24)
 static void ui_draw_cb(void);
+static void ui_save_settings_cb(ui_settings_t* settings);
 static void ui_boot_cb(cpc_t* sys, cpc_type_t type);
 static void ui_save_snapshot(size_t slot_index);
 static bool ui_load_snapshot(size_t slot_index);
@@ -177,6 +179,7 @@ void app_init(void) {
     #ifdef CHIPS_USE_UI
         ui_init(&(ui_desc_t){
             .draw_cb = ui_draw_cb,
+            .save_settings_cb = ui_save_settings_cb,
             .imgui_ini_key = "floooh.chips.cpc",
         });
         ui_cpc_init(&state.ui, &(ui_cpc_desc_t){
@@ -209,6 +212,7 @@ void app_init(void) {
                 .toggle_breakpoint = { .keycode = simgui_map_keycode(SAPP_KEYCODE_F9), .name = "F9" }
             }
         });
+        ui_cpc_load_settings(&state.ui, ui_settings());
         ui_load_snapshots_from_storage();
         // important: initialize webapi after ui
         webapi_init(&(webapi_desc_t){
@@ -443,6 +447,10 @@ static void draw_status_bar(void) {
 #if defined(CHIPS_USE_UI)
 static void ui_draw_cb(void) {
     ui_cpc_draw(&state.ui);
+}
+
+static void ui_save_settings_cb(ui_settings_t* settings) {
+    ui_cpc_save_settings(&state.ui, settings);
 }
 
 static void ui_boot_cb(cpc_t* sys, cpc_type_t type) {
